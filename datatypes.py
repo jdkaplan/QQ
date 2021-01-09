08683da3615b29e1a3f7ecd9d4dcf845ec828839
Jeremy Wright <jtwright@mit.edu>
2021-01-09 18:21:19 -0500

    added basic queue/block idea

--- datatypes.py
+++ datatypes.py
@@ -4,3 +4,28 @@ class Number:
 
     def execute(self, qframe):
         qframe.push(self)
+
+class String:
+    def __init__(self, s):
+        self.val = s
+
+    def execute(self, qframe):
+        qframe.push(self)
+
+class Block:
+    # TODO
+    def __init__(self, contents):
+        self.contents = contents
+
+    def execute(self, qframe):
+        qframe.push(Queue(self.contents))
+
+class Queue:
+    # TODO
+    def __init__(self, contents):
+        self.contents = contents
+
+    def execute(self, qframe):
+        while self.contents:
+            inst = self.contents.pop()
+            inst.execute(qframe)
