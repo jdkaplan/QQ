a550c38123b2b55547fb93f7610589f452cb3b45
Jeremy Wright <jtwright@mit.edu>
2021-01-09 19:26:36 -0500

    evaluator update

--- evaluator.py
+++ evaluator.py
@@ -1,17 +1,18 @@
 import sys
 
 from environment import Env
+import parser
 
 def repl():
     env = Env
     while (inp := input("> ")) != "QUIT!": # or whatever
-        inst = parse(inp)       # TODO
+        inst = parser.parse(inp)       # TODO
         inst.execute(env)
 
 def evaluate_file(fname):
     with open(fname) as f:
         env = Env
-        inst_q = parse(f.read()) # TODO
+        inst_q = parser.parse(f.read()) # TODO
         inst_q.execute(env) # assuming this will be a Queue for now
 
 if __name__ == '__main__':
