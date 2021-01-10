9ffe55d7ad766d8d2b1cf15a681117c58b16b43c
Jeremy Wright <jtwright@mit.edu>
2021-01-09 19:05:22 -0500

    typo in env fixed

--- environment.py
+++ environment.py
@@ -1,6 +1,6 @@
 from collections import deque
 
-def Env:
+class Env:
     def __init__(self, qframe=None, rqueue=None, fnqueue=None):
         self.qframe = qframe if qframe is not None else dequeue()
         self.rqueue = rqueue
