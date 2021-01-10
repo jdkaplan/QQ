9ed95823763423713986f9d0821177bde36aea90
Jeremy Kaplan <jdkaplan@metagram.net>
2021-01-09 20:17:14 -0800

    Remove unnecessary assert helper

--- parser_test.py
+++ parser_test.py
@@ -8,7 +8,7 @@ import math_operations
 
 class TestParser(unittest.TestCase):
     def test_empty(self):
-        self.assertQueue(parser.parse(""), parser.Queue([]))
+        self.assertEqual(parser.parse(""), parser.Queue([]))
 
     def test_identifiers(self):
         accepted = identifier.identifiers
@@ -16,7 +16,7 @@ class TestParser(unittest.TestCase):
             with self.subTest(ident):
                 actual = parser.parse(ident)
                 expected = parser.Queue([parser.Identifier(ident)])
-                self.assertQueue(actual, expected)
+                self.assertEqual(actual, expected)
 
     def test_unknown_identifier(self):
         with self.assertRaises(parser.VisitationError):
@@ -37,7 +37,7 @@ class TestParser(unittest.TestCase):
             with self.subTest(source):
                 actual = parser.parse(source)
                 expected = parser.Queue([parser.String(contents)])
-                self.assertQueue(actual, expected)
+                self.assertEqual(actual, expected)
 
         rejected = ['"unterminated string']
         for string in rejected:
@@ -51,7 +51,7 @@ class TestParser(unittest.TestCase):
             with self.subTest(number):
                 actual = parser.parse(number)
                 expected = parser.Queue([parser.Number(float(number))])
-                self.assertQueue(actual, expected)
+                self.assertEqual(actual, expected)
 
         rejected = ["0.", ".0"]
         for number in rejected:
@@ -78,7 +78,7 @@ class TestParser(unittest.TestCase):
                 math_operations.Sub(),
             ]
         )
-        self.assertQueue(actual, expected)
+        self.assertEqual(actual, expected)
 
     def test_simple_math_2(self):
         source = textwrap.dedent(
@@ -103,12 +103,12 @@ class TestParser(unittest.TestCase):
                 parser.Identifier("-"),
             ]
         )
-        self.assertQueue(actual, expected)
+        self.assertEqual(actual, expected)
 
     def test_single_number(self):
         actual = parser.parse("1")
         expected = parser.Queue([parser.Number(1)])
-        self.assertQueue(actual, expected)
+        self.assertEqual(actual, expected)
 
     def test_comment(self):
         source = textwrap.dedent(
@@ -118,12 +118,12 @@ class TestParser(unittest.TestCase):
         )
         actual = parser.parse(source)
         expected = parser.Queue([parser.Number(1), parser.Identifier("print")])
-        self.assertQueue(actual, expected)
+        self.assertEqual(actual, expected)
 
     def test_comments_are_not_identifiers(self):
         actual = parser.parse("# I am a comment")
         expected = parser.Queue([])
-        self.assertQueue(actual, expected)
+        self.assertEqual(actual, expected)
 
     def test_block(self):
         actual = parser.parse(textwrap.dedent("""\
@@ -136,11 +136,7 @@ class TestParser(unittest.TestCase):
                 parser.String("a"),
             ])
         ])
-        self.assertQueue(actual, expected)
-
-    def assertQueue(self, actual, expected):
-        assert type(actual) == type(expected), f"Expected #{type(expected)} got #{type(actual)}"
-        self.assertEqual(actual.statements, expected.statements)
+        self.assertEqual(actual, expected)
 
 
 if __name__ == "__main__":
